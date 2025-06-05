53324b6c35a0db434140367e0a7dcde93705dc19
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-06-05 14:07:11 -0400

    init example project and (finally) get environment + gradle working

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    fenix,
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    lib = pkgs.lib;
+
+    mcLibraries = with pkgs; [
+      libpulseaudio
+      libGL
+      glfw
+      openal
+      stdenv.cc.cc.lib
+      flite
+    ];
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = with pkgs; [alejandra jdk] ++ mcLibraries;
+      LD_LIBRARY_PATH = lib.makeLibraryPath mcLibraries;
+    };
+  };
+}

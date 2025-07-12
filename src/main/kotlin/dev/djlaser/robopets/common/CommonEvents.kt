9545f41e3124395a61321f7b4e3a23eb82efec7f
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-07-12 16:08:31 -0400

    Add event handler to allow opening gui instead of entity interaction (making pet sit)

--- src/main/kotlin/dev/djlaser/robopets/common/CommonEvents.kt
+++ src/main/kotlin/dev/djlaser/robopets/common/CommonEvents.kt
@@ -1,9 +1,24 @@
 package dev.djlaser.robopets.common
 
+import dev.djlaser.robopets.common.item.PetTransceiverItem
 import dev.djlaser.robopets.common.network.ServerBoundRenameEntityPayload
 import net.neoforged.bus.api.SubscribeEvent
 import net.neoforged.fml.common.EventBusSubscriber
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent
+
+@EventBusSubscriber(modid = RobopetsMod.MODID, bus = EventBusSubscriber.Bus.GAME)
+object CommonGameEvents {
+  @SubscribeEvent
+  @JvmStatic
+  fun interactWithEntity(event: PlayerInteractEvent.EntityInteractSpecific) {
+    val item = event.itemStack.item
+
+    if (item is PetTransceiverItem) {
+      item.handleInteractEvent(event)
+    }
+  }
+}
 
 @EventBusSubscriber(modid = RobopetsMod.MODID, bus = EventBusSubscriber.Bus.MOD)
 object CommonModEvents {
--- src/main/kotlin/dev/djlaser/robopets/common/item/PetTransceiverItem.kt
+++ src/main/kotlin/dev/djlaser/robopets/common/item/PetTransceiverItem.kt
@@ -2,6 +2,7 @@ package dev.djlaser.robopets.common.item
 
 import dev.djlaser.robopets.common.menu.PetTransceiverMenu
 import net.minecraft.network.chat.Component
+import net.minecraft.stats.Stats
 import net.minecraft.world.InteractionHand
 import net.minecraft.world.InteractionResult
 import net.minecraft.world.SimpleMenuProvider
@@ -9,8 +10,37 @@ import net.minecraft.world.entity.LivingEntity
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
+import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent
 
 class PetTransceiverItem(properties: Properties) : Item(properties) {
+  fun handleInteractEvent(event: PlayerInteractEvent.EntityInteractSpecific) {
+    val player = event.entity
+    val target = event.target
+    val hand = event.hand
+
+    if (target !is LivingEntity) {
+      return
+    }
+
+    val result = interactLivingEntity(
+      event.itemStack,
+      player,
+      target,
+      hand
+    )
+
+    event.cancellationResult = result
+    event.isCanceled = result != InteractionResult.PASS
+
+    if (result.shouldSwing() && event.level.isClientSide) {
+      player.swing(hand)
+    }
+
+    if (result.indicateItemUse()) {
+      player.awardStat(Stats.ITEM_USED[this])
+    }
+  }
+
   override fun interactLivingEntity(
     stack: ItemStack,
     player: Player,
@@ -18,6 +48,10 @@ class PetTransceiverItem(properties: Properties) : Item(properties) {
     usedHand: InteractionHand,
   ): InteractionResult {
     val level = player.level()
+
+    if (player.isSecondaryUseActive || usedHand == InteractionHand.OFF_HAND) {
+      return  InteractionResult.PASS
+    }
 
     if (!level.isClientSide()) {
       player.openMenu(
